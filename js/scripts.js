209cca76732dff84e3c4fda8340ad9a94ad5bf55
Curtis Williamson <cutty@Curtiss-MacBook-Pro.local>
2018-11-07 20:12:02 -0800

    dayTwo classwork done

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,66 @@
-alert('Welcome to JS');
+// var name = "Curtis";
+// a string is text or message to create a response "curtis"
+
+// var nameTwo = new string("Williamson");
+
+// var luckyNumber = 42;
+// number
+
+// var boolean = true;
+// boolean are yes or
+
+// let name = "Curtis";
+
+// {
+  // let name = "Paige";
+  // console.log(name);
+// }
+
+// console.log(name);
+
+// const greeting = "Hello my name is: ";
+//const name = "Bob";
+
+// console.log(`Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod ${name} tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
+
+// const date = new Date("09/27/89");
+
+// console.log(date);
+
+// console.log("asdlkfjalkfjalkrfmlkrcmlkerfmerfjaklsfkareklfare;klcmael;rfafj" .length)
+
+// const num = new Number(42);
+// const num2 = 42;
+
+// const array = [1, 2, 3];
+// const array2 = new Array(1, 2, 3)
+
+// console.log(array2);
+
+// const arrayMixed = ["ben", 29, true];
+
+// console.log(arrayMixed);
+
+// const obj = {
+  // name: "Curtis",
+  // age: 29
+// };
+
+// console.log(obj);
+
+// alert("Danger Will Robinson!!!");
+
+// const response = confirm("Are you sure?");
+
+// console.log(response);
+// records the response in the console
+
+// const favMovie = prompt("What is your favorite movie?");
+
+// console.log(favMovie);
+// records answer
+
+// const favNum = prompt("What is youyr favorite number?");
+// const realNum = parseInt(favNum);
+// console.log(typeof(realNum));
+// parseInt converts it to a real number
